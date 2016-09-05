b2f105d3cade6129a8cdafde8d5f37ec375e178f
khoobi <zahra.khoobi71@gmail.com>
2016-09-05 10:39:00 +0430

    add anotation for changing the status & read parameter from path

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -3,15 +3,25 @@ package com.example; /**
  */
 import org.json.JSONObject;
 import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Hello {
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public User index(@RequestBody User u2) {
-        User user1=new User();
-        user1.setName(u2.getName());
-        user1.setPass(u2.getPass());
+    //change status here
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping(value = "/user/{UserId}", method = RequestMethod.POST)
+    public User index(@RequestBody User u2,/*get parameter here*/@PathVariable("UserId") int id) {
+        User user1 = new User();
+        if(id==1) {
+            user1.setName(u2.getName());
+            user1.setPass(u2.getPass());
+
+        }
+        else{
+            user1.setName("12121");
+            user1.setPass("2323");
+        }
         return user1;
     }
 }
